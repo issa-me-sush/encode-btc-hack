fd0de596d6428dd6f7a03c3e7262a1986baf18df
issa-me-sush <coolsushaan@gmail.com>
2024-11-12 15:08:20 +0700

    fix

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -8,8 +8,8 @@ export function Auth() {
 
   if (!authenticated) {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-8 text-white">P2P Betting</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+        <h1 className="text-4xl font-bold mb-8 ">P2P Betting</h1>
         <button
           onClick={login}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
--- pages/bet/[id].tsx
+++ pages/bet/[id].tsx
@@ -134,7 +134,7 @@ export default function BetPage() {
   if (!authenticated) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-8">P2P Betting</h1>
+        <h1 className="text-4xl font-bold mb-8 ">P2P Betting</h1>
         <button
           onClick={login}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
